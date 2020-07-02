6ac1d6a854fd47bccc84d97316e42ae2da9430b3
viannaandreBR <andre@bioinformaticalabs.com>
2020-07-02 04:41:36 -0300

    app react-login OK!

--- src/components/User/Login/Login.jsx
+++ src/components/User/Login/Login.jsx
@@ -1,20 +1,71 @@
-import React from 'react';
-import UIButton from 'components/UI/Button/Button';
+import React, { useState, useContext } from "react";
+import { useHistory } from 'react-router-dom';
+import storeContext from 'components/Store/Context';
+import UIButton from "components/UI/Button/Button";
 
-import './Login.css';
+import "./Login.css";
+
+function initialState() {
+  return { user: "", password: "" };
+}
+
+function login({user,password}){
+  if (user == 'admin' && password=='admin') {
+    return { token: '1234' };
+  }
+    return { error: 'Usuário não autenticado'}
+}
+
 
 const UserLogin = () => {
+  const [values, setValues] = useState(initialState);
+  const { setToken } = useContext(storeContext);
+  const history = useHistory();
+
+  function onChange(event) {
+    const { value, name } = event.target;
+    setValues({
+      ...values,
+      [name]: value
+    });
+  }
+
+function onSubmit(event){
+  event.preventDefault();
+
+  const { token } = login(values);
+
+  if (token) {
+    setToken(token);
+    return history.push('/');
+  }
+
+  setValues(initialState);
+}
+
   return (
     <div className="user-login">
       <h1 className="user-login__title">Acessar o Sistema</h1>
-      <form autoComplete="nope">
+      <form onSubmit={onSubmit}>
         <div className="user-login__form-control">
-          <label htmlFor="email">E-mail</label>
-          <input id="email" type="text" name="email" autoComplete="off" />
+          <label htmlFor="user">E-mail</label>
+          <input
+            id="user"
+            type="text"
+            name="user"
+            onChange={onChange}
+            value={values.user}
+          />
         </div>
         <div className="user-login__form-control">
           <label htmlFor="password">Senha</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={onChange}
+            value={values.password}
+          />
         </div>
         <UIButton
           type="submit"
